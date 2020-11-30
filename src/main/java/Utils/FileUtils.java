65d1df1ae60ee32785325154db03e0e2473ff990
mwbouwkamp <marco.bouwkamp@timeseries.com>
2020-11-30 13:16:45 +0100

    Add FileUtils

---
+++ src/main/java/Utils/FileUtils.java
@@ -0,0 +1,40 @@
+package Utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileUtils {
+
+    private static final String PATH = "..\\resources\\";
+
+    public static List<String> getLinesFromInput(String fileName) throws FileNotFoundException {
+        return getBufferedReaderFromFile(fileName)
+                .lines()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> getIntegersFromInput(String fileName) throws FileNotFoundException {
+        return getBufferedReaderFromFile(fileName)
+                .lines()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Long> getLongsFromInput(String fileName) throws FileNotFoundException {
+        return getBufferedReaderFromFile(fileName)
+                .lines()
+                .mapToLong(Long::parseLong)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private static BufferedReader getBufferedReaderFromFile(String fileName) throws FileNotFoundException {
+        File file = new File(PATH + fileName);
+        return new BufferedReader((new FileReader(file)));
+    }
+}
